a5c2057ae83b9534cd89c4903267af22f4b5886d
annaliferous <anahita.marashi@hotmail.de>
2025-06-22 23:44:36 +0200

    first commit

---
+++ window.js
@@ -0,0 +1,16 @@
+
+let position = { x: 0, y: 0 };
+interact('.window-outline').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
